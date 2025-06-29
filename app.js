f703a53f35aacafad4d3468eb3500fc92d53245d
Amitoshverm <amitosh.verma9@gmail.com>
2025-06-30 01:48:12 +0530

    done

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
-{
-  /* <div>
-    <div>
-        <h1> nested child</h1>
-    </div>
-</div> */
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xyz: "abc" },
-//   "hello from react"
-// );
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child", key: "1" }, [
+//     React.createElement("h1", {}, "h1 tag from childrens"),
+//     React.createElement("h2", {}, "h2 tag from children"),
+//   ]),
+//   React.createElement("div", { id: "child", key: "2" }, [
+//     React.createElement("h1", {}, "h1 tag from children"),
+//     React.createElement("h2", {}, "h2 tag from children"),
+//   ]),
+// ]);
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "h1 tag from children"),
-    React.createElement("h2", {}, "h2 tag from children"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "h1 tag from children"),
-    React.createElement("h2", {}, "h2 tag from children"),
-  ]),
-]);
+// console.log(parent); // the parent here is not an html its an object and the react is using this object to create an html and render on dom
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent); // here
 
-console.log(parent); // the parent here is not an html its an object and the react is using this object to create an html and render on dom
+const heading = React.createElement("h2", { id: "heading" }, "hey hello"); // this is how we create element using react (which is an object).
+console.log(heading);
+
+// this is jsx, this is element creation using jsx
+const jsxHeading = <h1 id="heading"> This is jsx </h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); // here
+console.log(root);
+root.render(heading);
