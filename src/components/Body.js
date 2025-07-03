d54d01d7e0e3852a83a922b2009b2452fbde3d3f
Amitoshverm <amitosh.verma9@gmail.com>
2025-07-03 16:44:37 +0530

    skippping search bar and building filter rn

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,7 @@ import resList from "../utils/mockdata";
 const Body = () => {
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">filter</div>
       <div className="res-container">
         {resList.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
