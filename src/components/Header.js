531a7d9e1da4f5edec7085c74de33a8bb1e1c20b
Amitoshverm <amitosh.verma9@gmail.com>
2025-07-09 01:34:40 +0530

    Link is faster than anchor tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { LOGO_URL } from "../utils/constants";
 import { useState, useEffect } from "react";
 
@@ -15,9 +16,15 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
           <li>Cart</li>
           <button
             className="login"
