d5649990c6d02934bdffc018aa890d3eea2a8214
Amitoshverm <amitosh.verma9@gmail.com>
2025-07-16 17:56:31 +0530

    thats how usestate is created in class-component

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,13 +3,21 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
+
+    this.state = {
+      count: 0,
+      count2: 2,
+    };
   }
   render() {
     // destructing the prop
     const { name, location, contact } = this.props;
+    const { count, count2 } = this.state;
+
     return (
       <div className="user-card">
+        <h1>Count: {count}</h1>
+        <h1>Count2: {count2}</h1>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h3>Contack: {contact}</h3>
