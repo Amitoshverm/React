ef7a01801edd254eb3ce3530195797a9ab58b7af
Amitoshverm <amitosh.verma9@gmail.com>
2025-08-01 22:25:15 +0530

    building menu item in accordion fashion

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,6 +1,6 @@
-import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import useRestrauntMenu from "../utils/useRestaurantMenu";
+import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
@@ -12,7 +12,15 @@ const RestaurantMenu = () => {
   const { itemCards } =
     resData?.cards[4].groupedCard?.cardGroupMap?.REGULAR?.cards[2].card?.card ||
     {};
-  // console.log(itemCards);
+  // console.log(resData?.cards[4].groupedCard?.cardGroupMap?.REGULAR?.cards);
+
+  const categories =
+    resData?.cards[4].groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (c) =>
+        c.card?.card?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+  // console.log(categories);
 
   if (resData === null) {
     return <p>Loading...</p>;
@@ -22,11 +30,16 @@ const RestaurantMenu = () => {
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="bg-white rounded-lg shadow-lg p-6 w-3/4 md:w-1/2">
         <h1 className="font-bold text-2xl mb-4 text-center">{name}</h1>
-        <div className="flex justify-center mb-2">
+        <p className="text-gray-500 text-center mb-2">{costForTwoMessage}</p>
+
+        {/* categories accordion */}
+        {categories.map((category) => (
+          <RestaurantCategory data={category?.card?.card} />
+        ))}
+
+        {/* <div className="flex justify-center mb-2">
           <p className="font-bold text-gray-600">{cuisines.join(", ")}</p>
         </div>
-
-        <p className="text-gray-500 text-center mb-2">{costForTwoMessage}</p>
         <p className="text-yellow-500 text-center mb-4">{avgRating} stars</p>
         <h2 className="font-bold text-lg mb-4 text-center">Menu</h2>
         <ul className="space-y-2">
@@ -42,7 +55,7 @@ const RestaurantMenu = () => {
               </span>
             </li>
           ))}
-        </ul>
+        </ul> */}
       </div>
     </div>
   );
