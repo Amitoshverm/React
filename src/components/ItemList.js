edd8800d840fdf98cc1770459ded678b933e5bd3
Amitoshverm <amitosh.verma9@gmail.com>
2025-08-03 22:53:19 +0530

    accordion feature of menu list done

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,7 +1,6 @@
 import { CDN_URL } from "../utils/constants";
 
 const ItemList = ({ items }) => {
-  console.log(items);
   return (
     <div>
       {items.map((item) => (
@@ -11,12 +10,23 @@ const ItemList = ({ items }) => {
           <div className="w-9/12">
             <div className="py-2">
               <span className="text-left">{item.card.info.name}</span>
-              <span> - ₹ {item.card.info.price / 100}</span>
+              <span>
+                {" "}
+                - ₹{" "}
+                {item.card.info.price
+                  ? item.card.info.price / 100
+                  : item.card.info.defaultPrice / 100}
+              </span>
             </div>
             <p className="text-xs">{item.card.info.description}</p>
           </div>
 
           <div className="w-3/12 p-4">
+            <div className="absolute">
+              <button className="p-2 bg-black text-white mx-16 rounded-lg shadow-lg  ">
+                Add +
+              </button>
+            </div>
             <img
               className="w-full"
               src={CDN_URL + item.card.info.imageId}></img>
--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,18 +1,32 @@
 import ItemList from "./ItemList";
+import { useState } from "react";
+
 const RestaurantCategory = ({ data }) => {
-  //   console.log(data);
+  const [showItems, setShowItems] = useState(false);
+
+  const handleClick = () => {
+    console.log(showItems);
+    setShowItems(!showItems); // Toggle the state of showItems
+    // if (showItems === true) {
+    //   setShowItems(false);
+    // } else {
+    //   setShowItems(true);
+    // }
+  };
 
   return (
     <div>
       {/* Accordion Header */}
       <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4">
-        <div className=" flex justify-between">
+        <div
+          className=" flex justify-between cursor-pointer"
+          onClick={handleClick}>
           <span className="font-bold text-lg">
             {data.title} ({data.itemCards?.length})
           </span>
           <span>⬇️</span>
         </div>
-        <ItemList items={data.itemCards} />
+        {showItems && <ItemList items={data.itemCards} />}
       </div>
       {/* Accordian body */}
     </div>
